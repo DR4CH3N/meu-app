eab658aa7964c6ba117835b91c95886775d17112
Matheus Anthony <thehellc4t@gmail.com>
2022-10-10 11:52:48 -0300

    aprendendo a usar CSS com react

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,9 +1,16 @@
 import Menu from "../layout/Menu";
 
+const estilos = {
+  color: "white",
+  backgroundColor: "black",
+};
+
 const Cabecalho = () => {
+  // CSS in JS: estilos CSS embutidos no meio javascript
+  // CSS in line: (usa propriedade style e sintaxe de objeto)
   return (
-    <header>
-      <h1>Interface react</h1>
+    <header style={{ backgroundColor: "lightBlue", textAlign: "center" }}>
+      <h1 style={estilos}>Interface react</h1>
 
       <Menu />
     </header>
