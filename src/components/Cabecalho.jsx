320673c03a59cbc199bb9c257c5ca79d6fa49199
Matheus Anthony <thehellc4t@gmail.com>
2022-10-11 11:29:53 -0300

    exemplos de styled components

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,12 +1,38 @@
 import Menu from "../layout/Menu";
+import styled from "styled-components";
+
+const periodo = "diurno";
+
+/* tagged template para uso com styled components */
+const StyledCabecalho = styled.header`
+  /* caso o periodo for diurno, ele vai ficar azul claro, caso contrario ficara com cor azul escuro */
+  background-color: ${periodo === "diurno" ? "lightblue" : "darkblue"};
+  text-align: center;
+
+  h1 {
+    color: red;
+  }
+
+  p {
+    font-weight: bold;
+    color: blue;
+    font-size: 2rem;
+
+    &:hover {
+      color: red;
+      cursor: pointer;
+    }
+  }
+`;
 
 const Cabecalho = () => {
   return (
-    <header>
+    <StyledCabecalho>
       <h1>Interface react</h1>
 
       <Menu />
-    </header>
+      <p>TESTE!!</p>
+    </StyledCabecalho>
   );
 };
 export default Cabecalho;
