42a40f6ba0aadf374d98bbe142a141a1dc57616f
Matheus Anthony <thehellc4t@gmail.com>
2022-10-13 10:43:11 -0300

    Update Cabecalho.jsx

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,8 +1,15 @@
 import Menu from "../layout/Menu";
 
 const Cabecalho = () => {
+  const exemplo2 = () => {
+    console.log("exemplo2!");
+  };
+
   return (
     <header>
+      <button onClick={() => console.log("exemplo 1")}>Exemplo 1</button>
+      <button onClick={exemplo2}>Exemplo 2</button>
+      <hr />
       <h1>Interface react</h1>
 
       <Menu />
