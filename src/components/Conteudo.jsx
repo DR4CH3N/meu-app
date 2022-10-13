e19024929a0242fdafa8d6d719c73ee8daa029fb
Matheus Anthony <thehellc4t@gmail.com>
2022-10-13 10:01:09 -0300

    adicionando IDs e prop key pro map

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -3,18 +3,21 @@ import Artigo from "../layout/Artigo";
 const Conteudo = () => {
   const artigos = [
     {
+      id: 1,
       titulo: "artigo 1",
       subtitulo: "este é o subtitulo do artigo 1",
       texto: "este é o conteudo blah blah blah do artigo 1",
       curso: "JS avançado",
     },
     {
+      id: 2,
       titulo: "artigo 2",
       subtitulo: "este é o subtitulo do artigo 2",
       texto: "este é o conteudo sla do artigo 2",
       curso: "React",
     },
     {
+      id: 3,
       titulo: "artigo 3",
       subtitulo: "este é o subtitulo do artigo 3",
       texto: "este é o conteudo XYZ do artigo 3",
@@ -35,6 +38,7 @@ const Conteudo = () => {
         {artigos.map((artigo) => (
           // entre os parenteses abaixo ele esta fazendo um return, a versão atual é uma versão abreviada desta sintaxe
           <Artigo
+            key={artigo.id}
             titulo={artigo.titulo}
             subtitulo={artigo.subtitulo}
             curso={artigo.curso}
