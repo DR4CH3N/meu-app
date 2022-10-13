10886179ceb5f71e05317144bd947ef9e4b30d91
Matheus Anthony <thehellc4t@gmail.com>
2022-10-13 11:33:55 -0300

    mais exemplos de useState

--- src/components/Conteudo.jsx
+++ src/components/Conteudo.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
 import Artigo from "../layout/Artigo";
 
 const Conteudo = () => {
+  const [corFundo, setCorFundo] = useState();
+  const [contador, setContador] = useState(0);
+
+  const fundoAmarelo = () => setCorFundo("#FFEE71");
+
+  const fundoAzul = () => setCorFundo("lightblue");
+
+  const fundoVermelho = () => setCorFundo("lightpink");
+
+  const atualizaContagem = () => setContador(contador + 1);
   return (
-    <main>
+    <main style={{ backgroundColor: corFundo }}>
+      <button onMouseOver={fundoAmarelo}>Amarelo</button>
+      <button onMouseOver={fundoAzul}>Azul</button>
+      <button onMouseOver={fundoVermelho}>vermelho</button>
       <section>
-        <h2>Conteudo do site</h2>
+        <button onClick={atualizaContagem}>Contador: {contador} </button>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam
           beatae eos dolore quasi officia vel recusandae facere, quaerat quidem
